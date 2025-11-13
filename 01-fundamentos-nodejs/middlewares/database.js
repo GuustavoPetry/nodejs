4bd882d3bd1ca882b86e29079e9a7f99710b7b20
GuustavoPetry <ogustavopetry@gmail.com>
2025-11-13 10:41:00 -0300

    Criando persistência dos dados em arquivo JSON

--- 01-fundamentos-nodejs/middlewares/database.js
+++ 01-fundamentos-nodejs/middlewares/database.js
@@ -1,18 +1,48 @@
+import fs from "node:fs/promises";
+
+    // Define o caminho que o arquivo db.json será salvo
+    const databasePath = new URL("../db.json", import.meta.url);
+
 export class Database {
-    // # -> define uma propriedade de classe como Private, não pode ser acessada por outros arquivos
+    // # -> define uma propriedade ou método de classe como Private, não pode ser acessada por outros arquivos
     #database = {};
 
+    // Lê o arquivo db.json e atribui os valores na propriedade #database
+    // Caso o arquivo não exista, executa #persist() que cria o arquivo vazio
+    constructor() {
+        // Executa assim que a classe Database é instanciada
+        fs.readFile(databasePath, "utf-8")
+            .then(data => {
+                this.#database = JSON.parse(data);
+            })
+            .catch(() => {
+                this.#persist();
+            });
+    }
+
+    // Função para salvar os dados em arquivo JSON
+    #persist() {
+        fs.writeFile(databasePath, JSON.stringify(this.#database));
+    }
+
+    // Função que retorna todos os dados de uma determinada chave da propriedade #database
     select(table) {
         const data = this.#database[table] ?? [];
         return data;
     }
 
+    // Função que verifica se já existe a chave na propriedade #database e insere os dados
+    // Caso a chave ainda não exista cria e atribui os dados
     insert(table, data) {
         if(Array.isArray(this.#database[table])) {
             this.#database[table].push(data);
         } else {
             this.#database[table] = [data];
         }
+
+        // Persiste os dados em arquivo JSON
+        this.#persist();
+
         return data
     }
 }
